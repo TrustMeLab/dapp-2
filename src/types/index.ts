609f2f3d65919ed5882fad0aae4b4b79ccc6ace5
Quentin D.C <quent043@hotmail.com>
2023-04-29 10:00:22 +0100

    Added types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,7 @@
 import { ReactNode } from 'react'
 
 import type { NextPage } from 'next'
+import {Connector} from "wagmi";
 
 export enum PageAuth {
   Public, // Anyone can access
@@ -13,3 +14,30 @@ export type ExtendedPage<P = {}> = NextPage<P> & {
   getLayout?: (page: ReactNode) => ReactNode
   auth?: PageAuth
 }
+
+export type IUser = {
+  id: string;
+  handle: string;
+  address: string;
+  description?: IUserDetails;
+};
+
+export type IUserDetails = {
+  title: string;
+  name: string;
+  role: string;
+  image_url: string;
+  video_url?: string;
+  about: string;
+  skills_raw: string;
+};
+
+export type IAccount = {
+  address?: `0x${string}`;
+  connector?: Connector;
+  isConnecting: boolean;
+  isReconnecting: boolean;
+  isConnected: boolean;
+  isDisconnected: boolean;
+  status: 'connecting' | 'reconnecting' | 'connected' | 'disconnected';
+};
