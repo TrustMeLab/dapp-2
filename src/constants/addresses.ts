5084fdfe59b6ad8ef2275631f99940dc9c489d36
Quentin D.C <quent043@hotmail.com>
2023-04-30 16:04:58 +0100

    Updated addresses.ts config

--- src/constants/addresses.ts
+++ src/constants/addresses.ts
@@ -1,7 +1,32 @@
 import { chain } from 'wagmi'
 import { ChainMap } from './chains'
 
-export const TALENTLAYERID_ADDRESS: ChainMap = {
+export const ANYWHERE_ID_ADDRESS: ChainMap = {
+  [chain.hardhat.id]: '0x5fbdb2315678afecb367f032d93f642f64180aa3',
+  [chain.polygon.id]: '0xD7D1B2b0A665F03618cb9a45Aa3070f789cb91f2',
+  [chain.polygonMumbai.id]: '0x3F87289e6Ec2D05C32d8A74CCfb30773fF549306',
+}
+export const LEASE_ADDRESS: ChainMap = {
+  [chain.hardhat.id]: '0x5fbdb2315678afecb367f032d93f642f64180aa3',
+  [chain.polygon.id]: '0xD7D1B2b0A665F03618cb9a45Aa3070f789cb91f2',
+  [chain.polygonMumbai.id]: '0x3F87289e6Ec2D05C32d8A74CCfb30773fF549306',
+}
+export const PAYMENT_MANAGER_ADDRESS: ChainMap = {
+  [chain.hardhat.id]: '0x5fbdb2315678afecb367f032d93f642f64180aa3',
+  [chain.polygon.id]: '0xD7D1B2b0A665F03618cb9a45Aa3070f789cb91f2',
+  [chain.polygonMumbai.id]: '0x3F87289e6Ec2D05C32d8A74CCfb30773fF549306',
+}
+export const CROESUS_TOKEN_ADDRESS: ChainMap = {
+  [chain.hardhat.id]: '0x5fbdb2315678afecb367f032d93f642f64180aa3',
+  [chain.polygon.id]: '0xD7D1B2b0A665F03618cb9a45Aa3070f789cb91f2',
+  [chain.polygonMumbai.id]: '0x3F87289e6Ec2D05C32d8A74CCfb30773fF549306',
+}
+export const ORACLE_ADDRESS: ChainMap = {
+  [chain.hardhat.id]: '0x5fbdb2315678afecb367f032d93f642f64180aa3',
+  [chain.polygon.id]: '0xD7D1B2b0A665F03618cb9a45Aa3070f789cb91f2',
+  [chain.polygonMumbai.id]: '0x3F87289e6Ec2D05C32d8A74CCfb30773fF549306',
+}
+export const PLATFORM_ID_ADDRESS: ChainMap = {
   [chain.hardhat.id]: '0x5fbdb2315678afecb367f032d93f642f64180aa3',
   [chain.polygon.id]: '0xD7D1B2b0A665F03618cb9a45Aa3070f789cb91f2',
   [chain.polygonMumbai.id]: '0x3F87289e6Ec2D05C32d8A74CCfb30773fF549306',
