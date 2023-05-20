c44387d1955a28639582a6ae3c34e793773493c2
Quentin D.C <quent043@hotmail.com>
2023-05-20 08:56:08 +0100

    Missing footer

---
+++ src/components/layout/footer.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Container } from './container'
+
+export const Footer = () => {
+  return (
+    <footer>
+      <Container>
+        <div className="flex  items-center justify-center border-t border-base-300/50 py-6 text-sm">
+          <p className="order-1 basis-full text-center md:order-none md:basis-auto">
+            COPYRIGHT © 2023 Trust
+          </p>
+        </div>
+      </Container>
+    </footer>
+  )
+}
